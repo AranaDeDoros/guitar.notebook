82e1d7da8ecac2cfe73ed11af7918b8397cb3621
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-10-21 16:53:09 -0600

    defined DTOs

---
+++ src/main/java/com/arana/guitar/notebook/practice/dto/TabDTO.java
@@ -0,0 +1,13 @@
+package com.arana.guitar.notebook.practice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TabDTO {
+    private Long id;
+    private String url;
+}
