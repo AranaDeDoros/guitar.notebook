46ad8bf401bc196adf4ebd2b653e4194f7fd9dcd
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-10-21 16:54:22 -0600

    misc changes

--- src/main/java/com/arana/guitar/notebook/practice/config/SecurityConfig.java
+++ src/main/java/com/arana/guitar/notebook/practice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.arana.guitar.notebook.practice.models;
+package com.arana.guitar.notebook.practice.config;
 
+import com.arana.guitar.notebook.practice.models.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
