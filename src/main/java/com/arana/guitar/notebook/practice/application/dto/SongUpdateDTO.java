1e84e58dbd28c5219e65141141dc1081ef1a911b
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-10-23 23:52:02 -0600

    typo

--- src/main/java/com/arana/guitar/notebook/practice/application/dto/SongUpdateDTO.java
+++ src/main/java/com/arana/guitar/notebook/practice/application/dto/SongUpdateDTO.java
@@ -1,7 +1,7 @@
 package com.arana.guitar.notebook.practice.application.dto;
 
 public class SongUpdateDTO {
-    //for now, only the progress will be update
+    //for now, only the progress will be updated
     private String id;
     private int progress;
 }
