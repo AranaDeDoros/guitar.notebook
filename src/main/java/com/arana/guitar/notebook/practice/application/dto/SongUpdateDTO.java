ef5651243a6dfc24b2e5263b7e79b06e766f075b
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-10-26 00:06:08 -0600

    fixed dtos by adding imports hehe

--- src/main/java/com/arana/guitar/notebook/practice/application/dto/SongUpdateDTO.java
+++ src/main/java/com/arana/guitar/notebook/practice/application/dto/SongUpdateDTO.java
@@ -1,5 +1,11 @@
 package com.arana.guitar.notebook.practice.application.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongUpdateDTO {
     //for now, only the progress will be updated
     private String id;
