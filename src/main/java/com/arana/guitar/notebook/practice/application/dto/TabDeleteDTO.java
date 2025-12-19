0cc804f0867fb8c99f6b4da919a08d68e55e9b7e
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-19 00:01:00 -0600

    updated and added dtos

---
+++ src/main/java/com/arana/guitar/notebook/practice/application/dto/TabDeleteDTO.java
@@ -0,0 +1,11 @@
+package com.arana.guitar.notebook.practice.application.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TabDeleteDTO {
+    private String id;
+}
---
+++ src/main/java/com/arana/guitar/notebook/practice/application/dto/TabUpdateDTO.java
@@ -0,0 +1,13 @@
+package com.arana.guitar.notebook.practice.application.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TabUpdateDTO {
+    private String id;
+    private String url;
+    private String comment;
+}
